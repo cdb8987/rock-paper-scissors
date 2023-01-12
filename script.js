08bc4339b40f45ad7e0a4eac123979fe8b6a62e9
Charlie <cdb8987@gmail.com>
2023-01-12 18:06:15 -0500

    added several game functions
    function playgame, function evaluate outcome.  CURRENT BUGS:  onclick events are firing on page load.  Event listeners are firing on page load

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-let rockButton = document.getElementById('rock')
-let paperButton = document.getElementById('paper')
-let scissorsButton = document.getElementById('scissors')
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+const userScore = document.getElementById('userscore');
+const computerScore = document.getElementById('computerscore');
+const pl = 0;
+const cp = 0;
+userScore.innerText = `Your score ${pl}`
+computerScore.innerText = `CPU score ${cp}`
 
+function playGame(userChoice) {
+    const choiceList = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * choiceList.length)
+    const compChoice = choiceList[randomNumber]
+    console.log(userChoice, compChoice)
 
-function userSelection() {
-    document.getElementById('userinput').style.display = 'none'
-    console.log('function ran')
+    return evaluateOutcome(userChoice, compChoice)
+
 }
 
-rockButton.addEventListener('click', userSelection)
+function evaluateOutcome(userChoice, compChoice) {
+    let outcome;
+    if (userChoice == compChoice) { return [0, 0] }
+    else if (userChoice == 'rock' && compChoice == 'scissors') { return [1, 0] }
+    else if (userChoice == 'rock' && compChoice == 'paper') { return [0, 1] }
+    else if (userChoice == 'scissors' && compChoice == 'paper') { return [1, 0] }
+    else if (userChoice == 'scissors' && compChoice == 'rock') { return [0, 1] }
+    else if (userChoice == 'paper' && compChoice == 'rock') { return [1, 0] }
+    else if (userChoice == 'paper' && compChoice == 'scissors') { return [0, 1] }
+
+}
+
+
+
+rockButton.onclick = playGame('rock');
+paperButton.onclick = playGame('paper');
+scissorsButton.onclick = playGame('scissors');
+
+
+
